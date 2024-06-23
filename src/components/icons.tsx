d369bafbd93dd03ddd6b3de219d8901d03c5a4f1
naphattar <naphatchalermwai@gmail.com>
2024-06-23 14:30:18 +0700

    [Init] init project

---
+++ src/components/icons.tsx
@@ -0,0 +1,66 @@
+import {
+  LucideProps,
+  Moon,
+  SunMedium,
+  Instagram,
+  Facebook,
+  type LucideIcon,
+} from "lucide-react"
+import BackgroundImage from "@/assets/kaihorlogo.png";
+
+export type Icon = LucideIcon
+
+export const Icons = {
+  sun: SunMedium,
+  moon: Moon,
+  facebook : Facebook,
+  instagram: Instagram,
+  logo: (props: LucideProps) => (
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    aria-hidden="true"
+    role="img"
+    className="iconify iconify--logos"
+    width="31.88"
+    height="32"
+    preserveAspectRatio="xMidYMid meet"
+    viewBox="0 0 256 257"
+    {...props}
+  >
+    {/* Background Image */}
+    <image
+      href={BackgroundImage}
+      x="0"
+      y="0"
+      width="256"
+      height="257"
+      preserveAspectRatio="xMidYMid slice"
+    />
+
+    <defs>
+      <linearGradient
+        id="IconifyId1813088fe1fbc01fb466"
+        x1="-.828%"
+        x2="57.636%"
+        y1="7.652%"
+        y2="78.411%"
+      >
+        <stop offset="0%" stopColor="#41D1FF"></stop>
+        <stop offset="100%" stopColor="#BD34FE"></stop>
+      </linearGradient>
+      <linearGradient
+        id="IconifyId1813088fe1fbc01fb467"
+        x1="43.376%"
+        x2="50.316%"
+        y1="2.242%"
+        y2="89.03%"
+      >
+        <stop offset="0%" stopColor="#FFEA83"></stop>
+        <stop offset="8.333%" stopColor="#FFDD35"></stop>
+        <stop offset="100%" stopColor="#FFA800"></stop>
+      </linearGradient>
+    </defs>
+  </svg>
+  )
+}
---
+++ src/config/site.ts
@@ -0,0 +1,23 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "KH",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "About Us",
+      href: "/about",
+    },
+  ],
+  links: {
+    youtube: "https://youtube.com/@m6io",
+    facebook : "https://www.facebook.com/profile.php?id=61558699434541",
+    instagram : "https://www.instagram.com/kaihor.official/",
+    docs: "https://ui.shadcn.com",
+  },
+}
