57934db7db0a816965794c63fc9e007d0a74b37b
naphattar <naphatchalermwai@gmail.com>
2024-08-24 12:27:43 +0700

    Revert "[REMOVE] remove mode toggle"
    This reverts commit 4ef65d548bc69e7c031aced86f756e9e6177cb97.

--- src/components/Header/site-header.tsx
+++ src/components/Header/site-header.tsx
@@ -1,5 +1,6 @@
 import { Icons } from "@/components/Header/icons"
 import { MainNav } from "@/components/Header/main-nav"
+import { ModeToggle } from "@/components/Header/mode-toggle"
 import { buttonVariants } from "@/components/Header/ui/button"
 import { siteConfig } from "@/config/site"
 import { Link } from "react-router-dom"
@@ -37,6 +38,7 @@ export function SiteHeader() {
                 <span className="sr-only">Instagram</span>
               </div>
             </Link>
+            <ModeToggle />
           </nav>
         </div>
       </div>
