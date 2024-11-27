9ee43e0b5ccb9142f7f2114dd0847dd4bf084831
Naphat Chalermwai <naphatchalermwai@gmail.com>
2024-11-27 23:08:31 +0700

    Update ListView.tsx

--- src/pages/Camppage/components/ListView/ListView.tsx
+++ src/pages/Camppage/components/ListView/ListView.tsx
@@ -1,23 +1,26 @@
 import { CampData } from "@/types/camp";
 import CampCard from "./CampCard";
 
-interface ListViewProp{
-    campsList : CampData[]
+interface ListViewProp {
+    campsList: CampData[];
 }
-export default function ListView(props : ListViewProp){
+
+export default function ListView(props: ListViewProp) {
     return (
-    <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        {props.campsList.map((camp) => (
-            <CampCard
-                key={camp.campID}
-                id={camp.campID}
-                name={camp.name}
-                imgSrc={camp.imgSrc[0]}
-                location={camp.location}
-                director={camp.director}
-                date={camp.date}
-            />
-        ))}
-    </div>
-    )
+        <div className="h-[768px] overflow-y-scroll rounded-lg bg-gray-50 p-4 shadow-lg">
+            <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+                {props.campsList.map((camp) => (
+                    <CampCard
+                        key={camp.campID}
+                        id={camp.campID}
+                        name={camp.name}
+                        imgSrc={camp.imgSrc[0]}
+                        location={camp.location}
+                        director={camp.director}
+                        date={camp.date}
+                    />
+                ))}
+            </div>
+        </div>
+    );
 }
